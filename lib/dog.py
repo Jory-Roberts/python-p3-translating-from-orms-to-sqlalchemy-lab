3b642893855f5bad9b88e4b353c1cd972326c852
Jory Roberts <robertsjory13@gmail.com>
2023-07-26 21:53:00 -0500

    completes save create table

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,44 @@
+from sqlalchemy import create_engine
 from models import Dog
 
-def create_table(base):
-    pass
+SQLITE_URL = "sqlite:///:memory:"
+engine = create_engine(SQLITE_URL)
+
+
+def create_table(base, engine):
+    sql = """CREATE TABLE IF NOT EXISTS dogs(
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+        );
+        """
+    return engine
+
+
+create_table(Dog, engine)
+
 
 def save(session, dog):
-    pass
+    session.add(dog)
+    session.commit()
+    return session
+
 
 def get_all(session):
     pass
 
+
 def find_by_name(session, name):
     pass
+
 
 def find_by_id(session, id):
     pass
 
+
 def find_by_name_and_breed(session, name, breed):
     pass
+
 
 def update_breed(session, dog, breed):
     pass
